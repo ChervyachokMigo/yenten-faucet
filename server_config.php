36b223a2ae882020a92c1dd5d7bdf14bedb83053
Chervethread <74270643+ChervyachokMigo@users.noreply.github.com>
2021-03-04 19:55:14 +0500

    Update server_config.php

--- server_config.php
+++ server_config.php
@@ -1,4 +1,14 @@
 <?php
+/*
+Настройки RPC указываются в yenten/data/yenten.conf
+rpcuser=test
+rpcpassword=pasik
+stdinrpcpass=pasik
+rpcallowip=127.0.0.1
+server=1
+daemon=1
+rpcport=9982
+*/
 $GLOBALS["RPC_URL"] = 'http://test:pasik@127.0.0.1:9982';
 //коды для рекапчи http://www.google.com/recaptcha/admin
 $GLOBALS['RPC_RECAPTCHA_SITEKEY'] = "6LeDI3Eaцфкм34км234к";
