87c2658b0091e905a372a4a55e9916ede78a2900
Chervethread <74270643+ChervyachokMigo@users.noreply.github.com>
2021-03-05 01:01:14 +0500

    Add files via upload

--- faucet.php
+++ faucet.php
@@ -1,10 +1,10 @@
 <?php
 //данные для добавления в таблицу логов
-require_once("server_config.php");
+  require_once("server_config.php");
 
-$response = $_POST["g-recaptcha-response"];
+  $response = $_POST["g-recaptcha-response"];
 
-$url = 'https://www.google.com/recaptcha/api/siteverify';
+  $url = 'https://www.google.com/recaptcha/api/siteverify';
 	$data = array(
 		'secret' => $GLOBALS['RPC_RECAPTCHA_SECRETKEY'],
 		'response' => $response
@@ -30,54 +30,54 @@ $url = 'https://www.google.com/recaptcha/api/siteverify';
 
 	} elseif ($captcha_success->success==true) {
 
-require_once("jsonRPCClient.php");
-  //try{
-		$alt = new jsonRPCClient($GLOBALS["RPC_URL"]); 
-  //}
-  /*catch(Exception $e) {
-      $errors['RPCClient'] = "Нет соединения!";
-      $data['errors'] = true;
-      $data['errors']  = $errors;
-      echo json_encode($data);
-      die;
-  }*/
-$min = $GLOBALS["PAYOUT_MIN"];
-$max = $GLOBALS["PAYOUT_MAX"];
-$multi = (rand(50, 100) / 100) * 10;
-$amount = rand($min,$max)*$multi;
-$amount_max = $GLOBALS["PAYOUT_MAX"]*10;
-$amount=$amount/$GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"];
-		$username = $_POST['address'];
-		$check = $alt->validateaddress($username);
-
-		if($alt->getbalance() < $amount){
-  			$errors['balance'] = 'Кран пуст.';
-  			$data['errors'] = true;
-  			$data['errors']  = $errors;
-  			echo json_encode($data);
-        die;
-		}
-
-		else {
-        if($check->{'isvalid'} == 1){
-				
-            $alt->sendtoaddress($username, $amount);
-
-  					$data['success'] = true;
-            $chance = ($amount/$amount_max)*$GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"]*100;
-  					$data['boa'] = "Вы получили " . $amount . " енотов!<br>Мультикаст: ".$multi."x<br>Эффективность: ".$chance."%";
-            
-  					echo json_encode($data);
+      require_once("jsonRPCClient.php");
+        //try{
+      		$alt = new jsonRPCClient($GLOBALS["RPC_URL"]); 
+        //}
+        /*catch(Exception $e) {
+            $errors['RPCClient'] = "Нет соединения!";
+            $data['errors'] = true;
+            $data['errors']  = $errors;
+            echo json_encode($data);
             die;
-				}
-        
-        else {
-            $errors['address'] = 'Неправильный адрес.';
-  					$data['errors'] = true;
-  					$data['errors']  = $errors;
-  					echo json_encode($data);
-            die;
+        }*/
+      $min = $GLOBALS["PAYOUT_MIN"];
+      $max = $GLOBALS["PAYOUT_MAX"];
+      $multi = (rand(50, 100) / 100) * 10;
+      $amount = rand($min,$max)*$multi;
+      $amount_max = $GLOBALS["PAYOUT_MAX"]*10;
+      $amount=$amount/$GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"];
+      		$username = $_POST['address'];
+      		$check = $alt->validateaddress($username);
+
+      		if($alt->getbalance() < $amount){
+        			$errors['balance'] = 'Кран пуст.';
+        			$data['errors'] = true;
+        			$data['errors']  = $errors;
+        			echo json_encode($data);
+              die;
+
+      		} else {
+
+            if($check->{'isvalid'} == 1){
+    				
+                $alt->sendtoaddress($username, $amount);
+
+      					$data['success'] = true;
+                $chance = ($amount/$amount_max)*$GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"]*100;
+      					$data['boa'] = "Вы получили " . $amount . " енотов!<br>Мультикаст: ".$multi."x<br>Эффективность: ".$chance."%";
+                
+      					echo json_encode($data);
+                die;
+  				} else {
+                $errors['address'] = 'Неправильный адрес.';
+      					$data['errors'] = true;
+      					$data['errors']  = $errors;
+      					echo json_encode($data);
+                die;
+          }
+          
         }
-    }
 
-}
+    }
+?>
