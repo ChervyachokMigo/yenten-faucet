3b2d4c9bf2b6c15332bb004d55dc404e5b2e35a0
Chervethread <74270643+ChervyachokMigo@users.noreply.github.com>
2021-03-05 04:05:53 +0500

    Add files via upload

--- faucet.php
+++ faucet.php
@@ -46,11 +46,32 @@
       $multi_min = $GLOBALS["PAYOUT_MULTICAST_MIN"];
       $multi_max = $GLOBALS["PAYOUT_MULTICAST_MAX"];
 
+      $roll = rand( $min, $max );
       $multi = ( rand( $multi_min*10, $multi_max*10 ) / 100) * 10;
-      $amount = rand( $min, $max ) * $multi;
+
+      $amount = $roll * $multi;
       $amount_max = $max * $multi_max;
       $amount=$amount/$GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"];
 
+      $chance = round( ($amount/$amount_max)*$GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"]*100 , 0 ); //вычиисление процентов
+
+      $lucky_multi = 1;
+      if ($chance>=95) {
+      	$lucky_multi = $GLOBALS["PAYOUT_LUCKY_MULTIPLIER"];
+      }
+      $amount = $amount * $lucky_multi;
+      $chance = $chance * $lucky_multi;
+
+      $rare_multiplier = $GLOBALS["PAYOUT_RARE_MULTIPLIER"];
+      $isRare = 0;
+      $rare_chance = $GLOBALS["PAYOUT_RARE_CHANCE"];
+      $rare_roll = rand(1,10000);
+      	if ( ($rare_roll > (10000 - ( $rare_chance/2 ) * 100) ) || ( $rare_roll < ( $rare_chance/2 ) * 100 ) ){
+      		$isRare = 1;
+      		$amount = $amount * $rare_chance;
+      		$chance = $chance * $rare_chance;
+      	}
+      
       		$username = $_POST['address'];
       		$check = $alt->validateaddress($username);
 
@@ -68,8 +89,14 @@
                 $alt->sendtoaddress($username, $amount);
 
       					$data['success'] = true;
-                $chance = round( ($amount/$amount_max)*$GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"]*100 , 0 ); //вычиисление процентов
-      					$data['boa'] = "Вы получили " . round($amount,4) . " енотов!<br>Мультикаст: ".round($multi,1)."x<br>Удача: ".$chance."%";
+                
+      					$data['boa'] = "Вы получили " . round($amount,4) . " енотов!<br>Выпало: ".$roll."<br>Мультикаст: ".round($multi,1)."x<br>Удача: ".$chance."%";
+      					if ($chance>=95) {
+      						$data['boa'] .= " (x".$lucky_multi."!)<br>";
+      					}
+      					if ($isRare == 1){
+      						$data['boa'] .= "Невероятная удача!!! (х".$rare_multiplier.")<br>";
+      					}
                 
       					echo json_encode($data);
                 die;
