c96c4e922d7db58418c254512a97d8e152a417de
Chervethread <74270643+ChervyachokMigo@users.noreply.github.com>
2021-03-05 04:05:01 +0500

    Update server_config.php

--- server_config.php
+++ server_config.php
@@ -13,9 +13,12 @@ $GLOBALS["RPC_URL"] = 'http://test:pasik@127.0.0.1:9982';
 //коды для рекапчи http://www.google.com/recaptcha/admin
 $GLOBALS['RPC_RECAPTCHA_SITEKEY'] = "asdasdasasdassiLL";
 $GLOBALS['RPC_RECAPTCHA_SECRETKEY'] = 'asdasdsasasdasda';
-$GLOBALS["PAYOUT_MIN"] = 1;
-$GLOBALS["PAYOUT_MAX"] = 200;
-$GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"] = 10000;
-$GLOBALS["PAYOUT_MULTICAST_MIN"] = 4;
-$GLOBALS["PAYOUT_MULTICAST_MAX"] = 10;
+$GLOBALS["PAYOUT_MIN"] = 1;						//дефолтный ролл минимум
+$GLOBALS["PAYOUT_MAX"] = 200;					//дефолтный ролл максимум
+$GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"] = 10000;	//чем больше тем меньше
+$GLOBALS["PAYOUT_MULTICAST_MIN"] = 4;			//1-100
+$GLOBALS["PAYOUT_MULTICAST_MAX"] = 10;			//1-100
+$GLOBALS["PAYOUT_LUCKY_MULTIPLIER"] = 1.5;		//x1-infinity
+$GLOBALS["PAYOUT_RARE_MULTIPLIER"] = 5;			//x1-infinity
+$GLOBALS["PAYOUT_RARE_CHANCE"] = 3;				//1-100
 ?>
