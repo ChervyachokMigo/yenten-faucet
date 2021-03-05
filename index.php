44edc5fb26e3e960d10404597847b5ab508e6a27
Chervethread <74270643+ChervyachokMigo@users.noreply.github.com>
2021-03-05 06:10:14 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <title>Yenten coin - Yenten-pool.ml Faucet</title>
 <meta name="robots" content="noindex,nofollow, noodp,noydir"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="pragma" content="no-cache" />
+<meta http-equiv="cache-control" content="no-cache" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
