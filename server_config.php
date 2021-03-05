e4800291a293a574a06b2fb1f3408ede59785e7f
Chervethread <74270643+ChervyachokMigo@users.noreply.github.com>
2021-03-05 21:03:27 +0500

    Settings

--- server_config.php
+++ server_config.php
@@ -1,18 +1,31 @@
 <?php
 /*
 Настройки RPC указываются в yenten/data/yenten.conf
-rpcuser=test
-rpcpassword=pasik
-stdinrpcpass=pasik
+rpcuser=name
+rpcpassword=pass
+stdinrpcpass=pass
 rpcallowip=127.0.0.1
 server=1
 daemon=1
 rpcport=9982
 */
-$GLOBALS["RPC_URL"] = 'http://test:pasik@127.0.0.1:9982';
-//коды для рекапчи http://www.google.com/recaptcha/admin
-$GLOBALS['RPC_RECAPTCHA_SITEKEY'] = "asdasdasasdassiLL";
-$GLOBALS['RPC_RECAPTCHA_SECRETKEY'] = 'asdasdsasasdasda';
+/// Проверить работает ли сервер: 
+//           > yenten-cli.exe help
+////////////////////////////////////////////////////
+///// Настройки RPC сервера из yenten.conf ////////
+$GLOBALS["RPC_CONNECT_NAME"] = "name";
+$GLOBALS["RPC_CONNECT_PASSWORD"] = "pass";
+$GLOBALS["RPC_CONNECT_IP"] = "127.0.0.1";
+$GLOBALS["RPC_CONNECT_PORT"] = "9982";
+//////////////////////////////////////////
+///// Пароль от зашифрованого кошелька
+$GLOBALS["WALLET_PASS_PHRASE"] = "";		//оставьте пустую строку - "" если кошелек не зашифрован
+//////////////////////////////////////////
+///// Коды для рекапчи: http://www.google.com/recaptcha/admin
+$GLOBALS['RPC_RECAPTCHA_SITEKEY'] = "12312312312312312312321321321312312312312";
+$GLOBALS['RPC_RECAPTCHA_SECRETKEY'] = '123123123123123123123123-12312312312312';
+//////////////////////////////////////////
+//// Настройки выплат ////////
 $GLOBALS["PAYOUT_MIN"] = 10;						//дефолтный ролл минимум
 $GLOBALS["PAYOUT_MAX"] = 444;					//дефолтный ролл максимум
 $GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"] = 10000;	//чем больше тем меньше
@@ -24,7 +37,9 @@ $GLOBALS["PAYOUT_RARE_MULTIPLIER"] = 6;			//x1-infinity
 $GLOBALS["PAYOUT_RARE_CHANCE"] = 3;				//0.01-100%
 
 //////////////////////////////////////////////////////////////////////////////
-
+///// Подсчет, не конфигурировать ///////////////////////////////////////////
+$GLOBALS["RPC_URL"] = 	'http://'.$GLOBALS["RPC_CONNECT_NAME"].':'.$GLOBALS["RPC_CONNECT_PASSWORD"].
+						'@'.$GLOBALS["RPC_CONNECT_IP"].':'.$GLOBALS["RPC_CONNECT_PORT"];
 $all_max = $GLOBALS["PAYOUT_MAX"] * $GLOBALS["PAYOUT_MULTICAST_MAX"] * $GLOBALS["PAYOUT_LUCKY_MULTIPLIER"] * $GLOBALS["PAYOUT_RARE_MULTIPLIER"];
 $all_min = $GLOBALS["PAYOUT_MIN"] * $GLOBALS["PAYOUT_MULTICAST_MIN"];
 
