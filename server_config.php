df3ff919a8506fe7044ab502c677b35f310cd952
cpu_1 <74270643+ChervyachokMigo@users.noreply.github.com>
2021-03-09 16:17:00 +0500

    Update server_config.php

--- server_config.php
+++ server_config.php
@@ -17,10 +17,14 @@ $GLOBALS["RPC_CONNECT_PORT"] = "";
 //коды для рекапчи http://www.google.com/recaptcha/admin
 $GLOBALS['RPC_RECAPTCHA_SITEKEY'] = "";
 $GLOBALS['RPC_RECAPTCHA_SECRETKEY'] = '';
+// Пасс фраза для выплаты, если стоит
+$GLOBALS["WALLET_PASS_PHRASE"] = "";		//оставьте пустую строку - "" если кошелек не зашифрован
+// Пароль для админских модулей
+$GLOBALS["PETUX_PASSWORD"] = "FBSufYcBwE5d";
 /////////////////////////////////////////////////////////////////////////////////////
 // рейты для гемблинга
-$GLOBALS["PAYOUT_MIN"] = 100;	//100					//дефолтный ролл минимум
-$GLOBALS["PAYOUT_MAX"] = 444;//444					//дефолтный ролл максимум
+$GLOBALS["PAYOUT_MIN"] = 1;	//100					//дефолтный ролл минимум
+$GLOBALS["PAYOUT_MAX"] = 1;//444					//дефолтный ролл максимум
 $GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"] = 10000;	//чем больше тем меньше
 $GLOBALS["PAYOUT_MULTICAST_MIN"] = 0.2;			//1-100
 $GLOBALS["PAYOUT_MULTICAST_MAX"] = 15.0;			//1-100
@@ -31,10 +35,10 @@ $GLOBALS["PAYOUT_RARE_CHANCE"] = 6;				//0.01-100%
 // Когда выплачивать
 $GLOBALS["PAYOUT_LIMIT"] = 10;					//number of yentens to payout
 $GLOBALS["PAYOUT_AUTOPAY_LIMIT"] = 3;			//если выиграл (больше этого значения), то платим ему всё
-// Пасс фраза для выплаты, если стоит
-$GLOBALS["WALLET_PASS_PHRASE"] = "";		//оставьте пустую строку - "" если кошелек не зашифрован
 // для целочисленных базы
 $GLOBALS['DB_COINS_ACCURACCY'] = 100000;
+// пени
+$GLOBALS['FEE_AMOUNT'] = 0.00172;
 
 //////////////////////////////////////////////////////////////////////////////
 ///// Подсчет, не конфигурировать ///////////////////////////////////////////
@@ -44,4 +48,5 @@ $all_max = $GLOBALS["PAYOUT_MAX"] * $GLOBALS["PAYOUT_MULTICAST_MAX"] * $GLOBALS[
 $all_min = $GLOBALS["PAYOUT_MIN"] * $GLOBALS["PAYOUT_MULTICAST_MIN"];
 
 ///////////////////////////////////////////////////////////////////////////////
+
 ?>
