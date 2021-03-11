b6bf23004ced84170707c154413da3956f350775
cpu_1 <74270643+ChervyachokMigo@users.noreply.github.com>
2021-03-11 22:05:05 +0500

    Update server_config.php

--- server_config.php
+++ server_config.php
@@ -32,30 +32,40 @@ $GLOBALS['MYSQL_HOST'] = '127.0.0.1';
 $GLOBALS['MYSQL_PORT'] = '3306';
 $GLOBALS['MYSQL_USER'] = 'yenten_faucet';
 $GLOBALS['MYSQL_PASSWORD'] = 'TkvlQYeUMvqG';
+$GLOBALS['MYSQL_DB'] = '2ch_yenten_faucet';
 
 // для хранения в базе - точность целочисленных
 $GLOBALS['DB_COINS_ACCURACCY'] = 100000;
 
 // Стоимость комиссии за перевод
-$GLOBALS['FEE_AMOUNT'] = 0.00172;
+$GLOBALS['FEE_AMOUNT'] = 0.01;
 
 // Когда выплачивать
 $GLOBALS["PAYOUT_LIMIT"] = 10;					// количество накоплений для выплаты (в йентенах)
-$GLOBALS["PAYOUT_AUTOPAY_LIMIT"] = 3;			// считается выигрышем после этого значения (в йентенах)
-
+$GLOBALS["PAYOUT_WIN_LIMIT"] = 0.75;				// 1 - не выплачивать, 0 - выплачивать всегда
 
 
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////// рейты для гемблинга /////////////////////////////////////
-$GLOBALS["PAYOUT_MIN"] = 1;	//100					//дефолтный ролл минимум
-$GLOBALS["PAYOUT_MAX"] = 1;   //444					//дефолтный ролл максимум
+$GLOBALS["PAYOUT_MIN"] = 25;	//100					//дефолтный ролл минимум
+$GLOBALS["PAYOUT_MAX"] = 117;   //444					//дефолтный ролл максимум
 $GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"] = 10000;	//чем больше тем меньше
 $GLOBALS["PAYOUT_MULTICAST_MIN"] = 0.2;			//1-100
 $GLOBALS["PAYOUT_MULTICAST_MAX"] = 15.0;			//1-100
-$GLOBALS["PAYOUT_LUCKY_CHANCE_CAP"] = 65;		//0-100% 0 - выпадать всегда / 100% - никогда
+$GLOBALS["PAYOUT_LUCKY_CHANCE_CAP"] = 75;		//0-100% 0 - выпадать всегда / 100% - никогда
 $GLOBALS["PAYOUT_LUCKY_MULTIPLIER"] = 1.9;		//x1-infinity
 $GLOBALS["PAYOUT_RARE_MULTIPLIER"] = 4;			//x1-infinity
-$GLOBALS["PAYOUT_RARE_CHANCE"] = 6;				//0.01-100%
+$GLOBALS["PAYOUT_RARE_CHANCE"] = 3;				//0.01-100%
+
+//бонусы за капчу
+$GLOBALS["PAYOUT_MULTIPLIER_CAPTCHA_RATE"] = 0.00417;		//множитель за одну капчу
+$GLOBALS["PAYOUT_CAPTCHA_MAX_MULTIPLIER"] = 2;		// максимайльный множитель за капчи, 0 без лимита
+
+//бонус за людей на сайте
+$GLOBALS["PAYOUT_MIN_NUMBER_CAPTCHA"] = 16;		//минимально капчей, чтобы они начались считаться
+
+$GLOBALS["PAYOUT_RATE_PER_HUMAN"] = 0.066;	// 0 - inlinity (recomended)
+$GLOBALS["PAYOUT_MAX_MULTIPLIER_PER_HUMAN"] = 2;	// any, 0 - no limit
 
 
 
@@ -63,9 +73,14 @@ $GLOBALS["PAYOUT_RARE_CHANCE"] = 6;				//0.01-100%
 ///// Подсчет, не конфигурировать ///////////////////////////////////////////
 $GLOBALS["RPC_URL"] = 	'http://'.$GLOBALS["RPC_CONNECT_NAME"].':'.$GLOBALS["RPC_CONNECT_PASSWORD"].
 						'@'.$GLOBALS["RPC_CONNECT_IP"].':'.$GLOBALS["RPC_CONNECT_PORT"];
-$all_max = $GLOBALS["PAYOUT_MAX"] * $GLOBALS["PAYOUT_MULTICAST_MAX"] * $GLOBALS["PAYOUT_LUCKY_MULTIPLIER"] * $GLOBALS["PAYOUT_RARE_MULTIPLIER"];
+$all_max_without_captcha_online = $GLOBALS["PAYOUT_MAX"] * $GLOBALS["PAYOUT_MULTICAST_MAX"] * $GLOBALS["PAYOUT_LUCKY_MULTIPLIER"] * $GLOBALS["PAYOUT_RARE_MULTIPLIER"];
+$all_max = $GLOBALS["PAYOUT_MAX"] * $GLOBALS["PAYOUT_MULTICAST_MAX"] * $GLOBALS["PAYOUT_LUCKY_MULTIPLIER"] * $GLOBALS["PAYOUT_RARE_MULTIPLIER"] * $GLOBALS["PAYOUT_CAPTCHA_MAX_MULTIPLIER"] * $GLOBALS["PAYOUT_MAX_MULTIPLIER_PER_HUMAN"];
 $all_min = $GLOBALS["PAYOUT_MIN"] * $GLOBALS["PAYOUT_MULTICAST_MIN"];
+
+// Когда выплачивать (выигрыш)
+$GLOBALS["PAYOUT_AUTOPAY_LIMIT_MIN"] = ( $all_max_without_captcha_online * $GLOBALS["PAYOUT_WIN_LIMIT"] ) / $GLOBALS["PAYOUT_AMOUNT_MULTIPLIER"];			// считается выигрышем после этого значения (в йентенах)
 
 ///////////////////////////////////////////////////////////////////////////////
 
+?>
 ?>
